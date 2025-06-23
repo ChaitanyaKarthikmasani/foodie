3250412b4cb1803c8e54fdbb1aee2f316da779f9
Manideep kakkerla <manideepkakkerla2004@gmail.com>
2025-06-23 21:15:47 +0530

    minor fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-<<<<<<< HEAD
 // Use this to handle React Router on Render
-=======
->>>>>>> 700f22ab50fa22296361c699092950dbd4dff654
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
   },
-<<<<<<< HEAD
   base: '/', // <- IMPORTANT
-=======
->>>>>>> 700f22ab50fa22296361c699092950dbd4dff654
   server: {
     port: 5173,
   }
